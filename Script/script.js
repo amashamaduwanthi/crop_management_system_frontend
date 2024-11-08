749605dd572b8095f92baedb55f276488924f3c7
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-11-08 20:13:29 +0530

    create crop section

--- Script/script.js
+++ Script/script.js
@@ -1,13 +1,20 @@
 
 $("#dashboard-section").css({display: 'block'});
 $("#field-section").css({display:'none'});
+$("#crop-section").css({display:'none'});
 
 $('#nav-dashboard').on('click', () => {
     $('#dashboard-section').css({display: 'block'});
     $('#field-section').css({display: 'none'});
+    $("#crop-section").css({display:'none'});
 })
 
 $('#nav-field').on('click', () => {
     $('#field-section').css({display: 'block'});
     $('#dashboard-section').css({display: 'none'});
+})
+$('#nav-crop').on('click', () => {
+    $('#crop-section').css({display: 'block'});
+    $('#dashboard-section').css({display: 'none'});
+    $('#field-section').css({display: 'none'});
 })

25e5b04fce9f76c91f6558408f3d21f1de48282f
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-11-09 10:35:31 +0530

    create monitoring log

--- Script/script.js
+++ Script/script.js
@@ -5,6 +5,7 @@ $("#crop-section").css({display:'none'});
 $("#staff-section").css({display:'none'});
 $("#vehicle-section").css({display:'none'});
 $("#equipment-section").css({display:'none'});
+$("#monitoring-section").css({display:'none'});
 
 $('#nav-dashboard').on('click', () => {
     $('#dashboard-section').css({display: 'block'});
@@ -13,6 +14,7 @@ $('#nav-dashboard').on('click', () => {
     $("#staff-section").css({display:'none'});
     $("#vehicle-section").css({display:'none'});
     $("#equipment-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
 })
 
 $('#nav-field').on('click', () => {
@@ -22,6 +24,7 @@ $('#nav-field').on('click', () => {
     $("#crop-section").css({display:'none'});
     $("#vehicle-section").css({display:'none'});
     $("#equipment-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
 })
 $('#nav-crop').on('click', () => {
     $('#crop-section').css({display: 'block'});
@@ -30,6 +33,7 @@ $('#nav-crop').on('click', () => {
     $("#staff-section").css({display:'none'});
     $("#vehicle-section").css({display:'none'});
     $("#equipment-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
 })
 $('#nav-staff').on('click', () => {
     $("#staff-section").css({display:'block'});
@@ -38,24 +42,48 @@ $('#nav-staff').on('click', () => {
     $('#field-section').css({display: 'none'});
     $("#vehicle-section").css({display:'none'});
     $("#equipment-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
 
 })
 $('#nav-vehicle').on('click', () => {
     $("#vehicle-section").css({display:'block'});
-    $("#monitoring-section").css({display:'none'});
     $("#staff-section").css({display:'none'});
     $('#crop-section').css({display: 'none'});
     $('#dashboard-section').css({display: 'none'});
     $('#field-section').css({display: 'none'});
     $("#equipment-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
 });
 
 $('#nav-equipment').on('click', () => {
     $("#equipment-section").css({display:'block'});
     $("#vehicle-section").css({display:'none'});
-    $("#monitoring-section").css({display:'none'});
     $("#staff-section").css({display:'none'});
     $('#crop-section').css({display: 'none'});
     $('#dashboard-section').css({display: 'none'});
     $('#field-section').css({display: 'none'});
+    $("#monitoring-section").css({display:'none'});
 });
+
+$('#nav-monitoring-log').on('click', () => {
+    $("#equipment-section").css({display:'none'});
+    $("#vehicle-section").css({display:'none'});
+    $("#staff-section").css({display:'none'});
+    $('#crop-section').css({display: 'none'});
+    $('#dashboard-section').css({display: 'none'});
+    $('#field-section').css({display: 'none'});
+    $("#monitoring-section").css({display:'block'});
+});
+function togglePanel(button) {
+    const panelContent = button.nextElementSibling;
+    if (panelContent.style.display === "block") {
+        panelContent.style.display = "none";
+        button.textContent = "Show Details";
+    } else {
+        panelContent.style.display = "block";
+        button.textContent = "Hide Details";
+    }
+}
+
+
+
